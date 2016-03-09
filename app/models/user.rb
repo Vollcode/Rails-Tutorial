be75c61f787b83715baa5520fa94b37193da399e
Vollcode <daniel.ortiz.olivares@gmail.com>
2016-03-09 12:27:18 +0100

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length:  { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
       uniqueness: { case_sensitive: false }
   has_secure_password
-   validates :password, presence: true, length: { minimum: 6 }
+   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   # Returns the hash digest of the given string.
+
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -37,5 +38,4 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
-
 end
